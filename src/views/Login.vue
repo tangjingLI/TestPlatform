999cfa0916a0681623eb99ccbcbb5bad46f6c416
litangjing <181850074@smail.nju.edu.cn>
2022-11-20 23:12:56 +0800

    init

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+/**
+* @author ltj
+* @since  2022/11/20 22:51
+*/
+
+<template>
+  <div>hihihihihi</div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
